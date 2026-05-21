c01f872d0cd6fcb1b9155536339d770d23d77b67
schoolonlyaccount <macman5@tkf7.net>
2026-05-21 15:57:37 -0500

    Tweaks made to W03 Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,7 @@
 // W02
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
