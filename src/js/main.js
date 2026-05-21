967155fed2b6bd17fd6fbfdbbcb9dff368ee6b8e
derickshanana <derickshanana50@gmail.com>
2026-05-21 22:10:57 +0200

    w03 tam activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 
@@ -8,3 +11,5 @@ const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
+
+
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -36,3 +36,21 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
   }
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
+
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
+}
+
+export async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+export async function loadHeaderFooter() {
+  renderWithTemplate(await loadTemplate("../partials/header.html"), document.getElementById("main-header"));
+  renderWithTemplate(await loadTemplate("../partials/footer.html"),document.getElementById("main-footer"));
+}
