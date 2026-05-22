ad3b0015640fa14e5d72ed78374cb7c84e40f310
derickshanana <derickshanana50@gmail.com>
2026-05-22 14:21:33 +0200

    stretch activity: Refactored the Shopping Cart

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,34 @@
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+
+function cartItemTemplate(item) {
+    return `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img src="${item.Image}" alt="${item.Name}" />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+}
+
+export default class ShoppingCart {
+    constructor(listElement) {
+        this.listElement = listElement;
+    }
+
+    init() {
+        const cartItems = getLocalStorage("so-cart");
+        if (cartItems && cartItems.length > 0) {
+            this.renderCart(cartItems);
+        } else {
+            this.listElement.innerHTML = "<li class='cart-card'>Your cart is empty.</li>";
+        }
+    }
+
+    renderCart(cartItems) {
+        renderListWithTemplate(cartItemTemplate, this.listElement, cartItems);
+    }
+}
--- src/js/cart.js
+++ src/js/cart.js
@@ -1,33 +1,8 @@
-import { getLocalStorage } from "./utils.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
 loadHeaderFooter();
 
-function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  if (cartItems !== null) {
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  }
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
-renderCartContents();
+const cartElement = document.querySelector(".product-list");
+const cart = new ShoppingCart(cartElement);
+cart.init();
